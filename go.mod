588932c260701e38d6eede7e9b441f3d38641bfb
aluriDevAnanth <aluridevananth@gmail.com>
2024-12-07 17:52:42 +0530

    init commit

---
+++ go.mod
@@ -0,0 +1,33 @@
+module zocket
+
+go 1.23.4
+
+require (
+	github.com/gorilla/mux v1.8.1
+	gorm.io/driver/postgres v1.5.11
+	gorm.io/gorm v1.25.12
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davidbyttow/govips/v2 v2.15.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/image v0.10.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,330 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"mime"
+
+	"github.com/disintegration/imaging"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/lib/pq"
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID                      uint           `gorm:"primaryKey;autoIncrement" json:"id"`
+	UserID                  uint           `json:"user_id"`
+	ProductName             string         `json:"product_name"`
+	ProductDescription      string         `json:"product_description"`
+	ProductImages           pq.StringArray `gorm:"type:text[]" json:"product_images"`
+	CompressedProductImages pq.StringArray `gorm:"type:text[]" json:"compressed_product_images"`
+	ProductPrice            float64        `json:"product_price"`
+}
+
+type User struct {
+	ID   uint   `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name string `json:"name"`
+}
+
+var db *gorm.DB
+var err error
+var logger = logrus.New()
+
+func main() {
+	// Setup logger
+	logger.SetFormatter(&logrus.JSONFormatter{})
+	logger.SetOutput(os.Stdout)
+	logger.SetLevel(logrus.InfoLevel)
+
+	dsn := "host=localhost user=postgres password=qqq dbname=test port=5432 sslmode=disable"
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		logger.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&Product{}, &User{}); err != nil {
+		logger.Fatalf("Failed to migrate database schema: %v", err)
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/products", createProduct).Methods("POST")
+	router.HandleFunc("/products/{id}", getProduct).Methods("GET")
+	router.HandleFunc("/products", getProducts).Methods("GET")
+
+	logger.Info("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	var product Product
+	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("Invalid request payload")
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	// Log request details
+	logger.WithFields(logrus.Fields{
+		"user_id":      product.UserID,
+		"product_name": product.ProductName,
+	}).Info("Creating product")
+
+	// Process the images (download and compress them)
+	originalImagePaths, compressedImagePaths, err := processImages(product.ProductImages)
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("Image processing error")
+		http.Error(w, "Failed to process images", http.StatusInternalServerError)
+		return
+	}
+
+	// Assign the image paths to the product
+	product.ProductImages = pq.StringArray(originalImagePaths)
+	product.CompressedProductImages = pq.StringArray(compressedImagePaths)
+
+	fixFilePaths(&product)
+
+	// Insert the product into the database
+	if err := db.Create(&product).Error; err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("Database error while creating product")
+		http.Error(w, "Failed to create product", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(product)
+
+	// Log success
+	logger.WithFields(logrus.Fields{
+		"id":      product.ID,
+		"user_id": product.UserID,
+	}).Info("Product created successfully")
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	var product Product
+	if err := db.First(&product, id).Error; err != nil {
+		logger.WithFields(logrus.Fields{
+			"product_id": id,
+			"error":      err,
+		}).Error("Product not found")
+		http.Error(w, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	fixFilePaths(&product)
+
+	// Log successful retrieval
+	logger.WithFields(logrus.Fields{
+		"id": id,
+	}).Info("Product retrieved successfully")
+
+	json.NewEncoder(w).Encode(product)
+}
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	userID := r.URL.Query().Get("user_id")
+	minPrice := r.URL.Query().Get("min_price")
+	maxPrice := r.URL.Query().Get("max_price")
+	name := r.URL.Query().Get("product_name")
+
+	var products []Product
+	query := db.Model(&Product{})
+
+	if userID != "" {
+		query = query.Where("user_id = ?", userID)
+	}
+	if minPrice != "" {
+		min, _ := strconv.ParseFloat(minPrice, 64)
+		query = query.Where("product_price >= ?", min)
+	}
+	if maxPrice != "" {
+		max, _ := strconv.ParseFloat(maxPrice, 64)
+		query = query.Where("product_price <= ?", max)
+	}
+	if name != "" {
+		query = query.Where("product_name ILIKE ?", "%"+name+"%")
+	}
+
+	if err := query.Find(&products).Error; err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("Failed to retrieve products")
+		http.Error(w, "Failed to retrieve products", http.StatusInternalServerError)
+		return
+	}
+
+	// Log successful retrieval of products
+	logger.WithFields(logrus.Fields{
+		"count": len(products),
+	}).Info("Products retrieved successfully")
+
+	for i := range products {
+		for j := range products[i].ProductImages {
+			products[i].ProductImages[j] = strings.ReplaceAll(products[i].ProductImages[j], "\\", "/")
+		}
+	}
+
+	for i := range products {
+		for j := range products[i].CompressedProductImages {
+			products[i].CompressedProductImages[j] = strings.ReplaceAll(products[i].CompressedProductImages[j], "\\", "/")
+		}
+	}
+
+	json.NewEncoder(w).Encode(products)
+}
+
+func downloadImage(url string, destPath string) (string, string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"url":   url,
+			"error": err,
+		}).Error("Failed to download image")
+		return "", "", fmt.Errorf("failed to download image: %w", err)
+	}
+	defer resp.Body.Close()
+
+	contentType := resp.Header.Get("Content-Type")
+	ext, _ := mime.ExtensionsByType(contentType)
+	if len(ext) == 0 {
+		logger.WithFields(logrus.Fields{
+			"url":          url,
+			"content_type": contentType,
+		}).Error("Failed to detect file extension")
+		return "", "", fmt.Errorf("failed to detect file extension for content type: %s", contentType)
+	}
+	extension := ext[len(ext)-1]
+
+	fileNameRand := strings.ReplaceAll(uuid.New().String(), "-", "")
+	randomizedFileName := fmt.Sprintf("%s%s", fileNameRand, extension)
+	randomizedCompressedFileName := fmt.Sprintf("%s%s", fileNameRand, extension)
+
+	destPathOrg := filepath.Join("images", randomizedFileName)
+	destPathCompressed := filepath.Join("compressed_images", randomizedCompressedFileName)
+
+	outFile, err := os.Create(destPathOrg)
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"url":   url,
+			"error": err,
+		}).Error("Failed to create image file")
+		return "", "", fmt.Errorf("failed to create image file: %w", err)
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"url":   url,
+			"error": err,
+		}).Error("Failed to save image")
+		return "", "", fmt.Errorf("failed to save image: %w", err)
+	}
+
+	logger.WithFields(logrus.Fields{
+		"url":         url,
+		"destination": destPathOrg,
+	}).Info("Image downloaded successfully")
+
+	return destPathOrg, destPathCompressed, nil
+}
+
+func compressImage(inputPath, outputPath string) error {
+	img, err := imaging.Open(inputPath)
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"input_path": inputPath,
+			"error":      err,
+		}).Error("Failed to open image for compression")
+		return fmt.Errorf("failed to open image: %w", err)
+	}
+
+	ext := filepath.Ext(inputPath)
+	if ext != ".jpg" && ext != ".jpeg" && ext != ".png" && ext != ".gif" {
+		logger.WithFields(logrus.Fields{
+			"input_path": inputPath,
+			"extension":  ext,
+		}).Error("Unsupported image format")
+		return fmt.Errorf("unsupported image format: %s", ext)
+	}
+
+	img = imaging.Resize(img, 800, 0, imaging.Lanczos)
+
+	if ext == ".png" {
+		err = imaging.Save(img, outputPath)
+	} else {
+		err = imaging.Save(img, outputPath, imaging.JPEGQuality(80))
+	}
+
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"input_path":  inputPath,
+			"output_path": outputPath,
+			"error":       err,
+		}).Error("Failed to save compressed image")
+		return fmt.Errorf("failed to save compressed image: %w", err)
+	}
+
+	logger.WithFields(logrus.Fields{
+		"input_path":  inputPath,
+		"output_path": outputPath,
+	}).Info("Image compressed successfully")
+
+	return nil
+}
+
+func processImages(imageUrls []string) ([]string, []string, error) {
+	var originalImagePaths, compressedImagePaths []string
+
+	os.MkdirAll("images", os.ModePerm)
+	os.MkdirAll("compressed_images", os.ModePerm)
+
+	for _, url := range imageUrls {
+		originalPath := filepath.Join("images", uuid.New().String())
+
+		originalPath, compressedPath, err := downloadImage(url, originalPath)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		err = compressImage(originalPath, compressedPath)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		originalImagePaths = append(originalImagePaths, originalPath)
+		compressedImagePaths = append(compressedImagePaths, compressedPath)
+	}
+
+	return originalImagePaths, compressedImagePaths, nil
+}
+
+func fixFilePaths(product *Product) {
+	for i, path := range product.ProductImages {
+		product.ProductImages[i] = strings.ReplaceAll(path, "\\", "/")
+	}
+
+	for i, path := range product.CompressedProductImages {
+		product.CompressedProductImages[i] = strings.ReplaceAll(path, "\\", "/")
+	}
+}
